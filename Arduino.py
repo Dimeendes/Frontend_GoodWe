ad65623890d0e88a12bf29d680f979b40163c110
dimeendes <diegomendesantonio@gmail.com>
2025-10-26 09:55:57 -0300

    Adicionar arquivos Python do Arduino e atualizar .gitignore

---
+++ Arduino.py
@@ -0,0 +1,15 @@
+import serial
+
+conexao = " "
+for porta in range(10):
+    try:
+        conexao = serial.Serial("COM" + str(porta), 115200, timeout = 0.5)
+        print("Conectado na porta:  ", conexao.portstr)
+        break
+    except serial.SerialException:
+        pass
+if conexao != " ":
+    conexao.close()
+    print("Conexão enceerrada")
+else:
+    print("Sem portas disponíveis")
